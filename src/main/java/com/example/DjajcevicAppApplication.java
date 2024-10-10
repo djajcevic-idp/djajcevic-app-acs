902764250192aadfcb1d3f12e0575bae8a15a154
root <root@djajcevic-app-acs-bootstrap-sgth8>
2024-10-10 20:28:33 +0000

    Initial commit

---
+++ src/main/java/com/example/DjajcevicAppApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DjajcevicAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DjajcevicAppApplication.class, args);
+	}
+
+}
